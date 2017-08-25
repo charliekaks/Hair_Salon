11cedf08337c7375f8407c35c3144ba7fc3851c5
charliekaks <kaksunchained@gmail.com>
2017-08-25 11:00:53 +0300

    Intial commit
    intialized the project, added the neccesary dependencies and the first tests

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,9 @@
+// import org.junit.Test;
+// import static org.junit.Assert.*;
+// import org.sql2o.*;
+
+// public class ClientTest{
+
+//     @Test
+//     public void c_instantiatesCorrectly_true()
+// }
