04925360b678bf563b57c448e9665e5c9c2f5a24
charliekaks <kaksunchained@gmail.com>
2017-08-25 15:42:25 +0300

    added forms to fill in client details

---
+++ src/main/java/App.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.Map;
+
+import org.apache.velocity.Template;
+
+import java.util.ArrayList;
+public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        // get("/", (request, response) ->{
+        //     Map<String, Object> model = new HashMap<String, Object>();
+        //     model.put("template", "templates/index.vtl");
+        //     return new ModelAndView(model, layout);
+        // },new VelocityTemplateEngine());
+
+        get("/", (request, response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("stylists",Stylist.all());
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        },new VelocityTemplateEngine());
+
+
+
+        post("/stylist", (request, response)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("stylistName");
+            int phone = Integer.parseInt(request.queryParams("phoneNumber"));
+            Stylist myStylist = new Stylist(name, phone);
+            myStylist.save();
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        },new VelocityTemplateEngine());
+
+        
+    }
+}
---
+++ src/main/java/Stylist.java
@@ -0,0 +1,86 @@
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Stylist{
+    private String stylistName;
+    private int phoneNumber;
+    private int id;
+
+
+    //method to overide the equals method
+    @Override
+    public boolean equals(Object otherStylist){
+        if (!(otherStylist instanceof Stylist)) {
+            return false;
+        }else{
+            Stylist newStylist = (Stylist) otherStylist;
+            return this.getName().equals(newStylist.getName())&&
+            this.getNumber()==newStylist.getNumber();
+        }
+
+    }
+
+    public Stylist(String name, int number){
+        this.stylistName = name;
+        this.phoneNumber = number;
+    }
+
+    public String getName(){
+        return stylistName;
+    }
+
+    public int getNumber(){
+        return phoneNumber;
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO stylists (stylistName, phoneNumber) VALUES (:stylistName, :phoneNumber)";
+            this.id = (int) con.createQuery(sql, true)
+            .addParameter( "stylistName", this.stylistName)
+            .addParameter("phoneNumber", this.phoneNumber)
+            .executeUpdate()
+            .getKey();
+        }
+    }
+
+    public static List<Stylist>all(){
+        String sql = "SELECT * FROM stylists";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+
+    public static Stylist find(int id){
+        try(Connection con = DB.sql2o.open()){
+        String sql = "SELECT id, stylists Where id=:id";
+        Stylist mystylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Stylist.class);
+        return mystylist;
+    }
+    }
+
+    public void update(String stylistName, int phone){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "UPDATE clients SET stylistName= :stylistName, phoneNumber=:phoneNumber WHERE id=:id";
+            con.createQuery(sql)
+            .addParameter( "stylistName", stylistName)
+            .addParameter("phonNumber", phoneNumber)
+            .addParameter("id",id)
+            .executeUpdate();
+        }
+
+    }
+
+    public void delete(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM stylists WHERE id=:id";
+            con.createQuery(sql)
+            .addParameter("id", id)
+            .executeUpdate();
+        }
+    }
+}
